afa929aac2c800104991b4bde61a68896d8e168e
Lucas-Pardo <107495790+Lucas-Pardo@users.noreply.github.com>
2025-03-06 20:28:33 +0100

    Initial commit

---
+++ exercise2.py
@@ -0,0 +1,161 @@
+from environments.environment3 import Environment as Env
+from robots import Robot, StickRobot
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+# Initialize the environment
+env = Env([1 for _ in range(4)], sigma=5)  # Set number of obstacles and sigma
+env_limit = 10
+# Spawn a robot at random position
+# x, y = 4.5, -4.5
+x, y = 0, 0
+while True:
+    x = np.random.uniform(-env_limit//2, env_limit//2)
+    y = np.random.uniform(-env_limit//2, env_limit//2)
+    if not env.is_collision(x, y, 3):
+        break
+
+robot_type = 2
+robot_length = 1.5
+if robot_type == 1:
+    robot = Robot(x, y)
+    path = [[robot.x, robot.y]]
+    input_hist = [[0, 0]]
+else:
+    theta = np.random.uniform(0, np.pi/2)
+    robot = StickRobot(x, y, theta, robot_length)
+    path = [[robot.x, robot.y, robot.theta]]
+    input_hist = [[0, 0, 0]]
+    
+# Simulate the robot movement
+dt = 0.05
+grad_hist = [[0, 0]]
+for _ in range(1500):
+    if robot_type == 1:
+        x, y = robot.bb_grad_descent(env)
+        robot.apf_update(dt, env, goal=[x, y], k=0.5, k_rep=1.5, gamma=2, rho=2, eta_0=5, nu=0.8)
+        path.append([robot.x, robot.y])
+        grad_hist.append(robot.prev_dh)
+        input_hist.append([robot.u_x, robot.u_y])
+    else:
+        x, y = robot.bb_grad_descent(env)
+        robot.apf_update(dt, env, goal=[x, y], k=0.5, k_rep=0.6, gamma=2, rho=2, eta_0=3, nu=0.8, mul_theta=2.5, tail_orientation=0)
+        path.append([robot.x, robot.y, robot.theta])    
+        grad_hist.append(robot.prev_dh)
+        input_hist.append([robot.u_x, robot.u_y, robot.u_theta]) 
+    if robot.prev_dh is not None:
+        if np.sqrt(robot.prev_dh[0]**2 + robot.prev_dh[1]**2) < 1e-2:
+            break
+
+# Plot the robot path using matplotlib
+path = np.array(path)
+grad_hist = np.array(grad_hist)
+input_hist = np.array(input_hist)
+
+# Create subplots with a grid specification
+fig = plt.figure(figsize=(14, 8))
+gs = fig.add_gridspec(2, 2, width_ratios=[3, 1])
+
+ax1 = fig.add_subplot(gs[:, 0])  # Main animation plot
+ax2 = fig.add_subplot(gs[0, 1])  # Inputs subplot
+ax3 = fig.add_subplot(gs[1, 1])  # Path subplot
+
+# Plot the environment data using matplotlib
+x = np.linspace(-env_limit, env_limit, 100)
+y = np.linspace(-env_limit, env_limit, 100)
+X, Y = np.meshgrid(x, y)
+Z = np.vectorize(env.h)(X, Y)
+contourf = ax1.contourf(X, Y, Z, levels=20, cmap='viridis')
+fig.colorbar(contourf, ax=ax1, orientation='vertical', label='H(x, y)')
+
+for diameter, x, y in env.obstacles:
+    circle = plt.Circle((x, y), diameter / 2, color='blue', fill=True)
+    ax1.add_patch(circle)
+
+# Add a line to represent the path
+path_line, = ax1.plot([], [], 'r--', alpha=0.25)
+
+# Initialize input and path subplots
+input_line_x, = ax2.plot([], [], 'b-', label='u_x', alpha=0.5)
+input_line_y, = ax2.plot([], [], 'r-', label='u_y', alpha=0.5)
+grad_line_x, = ax2.plot([], [], 'c-', label='grad_x', alpha=0.5)
+grad_line_y, = ax2.plot([], [], 'm-', label='grad_y', alpha=0.5)
+if robot_type == 2:
+    input_line_theta, = ax2.plot([], [], 'g-', label='u_theta', alpha=0.5)
+ax2.legend()
+ax2.set_xlim(0, len(input_hist))
+ax2.set_ylim(1.2*np.min(input_hist), 1.2*np.max(input_hist))
+ax2.set_xlabel('Time step')
+ax2.set_ylabel('Input')
+
+path_line_x, = ax3.plot([], [], 'b-', label='x', alpha=0.5)
+path_line_y, = ax3.plot([], [], 'r-', label='y', alpha=0.5)
+if robot_type == 2:
+    path_line_theta, = ax3.plot([], [], 'g-', label='theta', alpha=0.5)
+ax3.legend()
+ax3.set_xlim(0, len(path))
+ax3.set_ylim(1.2*np.min(path), 1.2*np.max(path))
+ax3.set_xlabel('Time step')
+ax3.set_ylabel('Position')
+
+def update_r1(num, path, robot_circle, grad_arrows, input_arrows, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y):
+    robot_circle.center = (path[num, 0], path[num, 1])
+    grad_arrows.set_offsets([path[num, 0], path[num, 1]])
+    grad_arrows.set_UVC(grad_hist[num, 0], grad_hist[num, 1])
+    input_arrows.set_offsets([path[num, 0], path[num, 1]])
+    input_arrows.set_UVC(input_hist[num, 0], input_hist[num, 1])
+    path_line.set_data(path[:num+1, 0], path[:num+1, 1])
+    input_line_x.set_data(range(num+1), input_hist[:num+1, 0])
+    input_line_y.set_data(range(num+1), input_hist[:num+1, 1])
+    grad_line_x.set_data(range(num+1), grad_hist[:num+1, 0])
+    grad_line_y.set_data(range(num+1), grad_hist[:num+1, 1])
+    path_line_x.set_data(range(num+1), path[:num+1, 0])
+    path_line_y.set_data(range(num+1), path[:num+1, 1])
+    return grad_arrows, input_arrows, robot_circle, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y
+
+def update_r2(num, path, stick_robot, robot_circle, grad_arrows, input_arrows, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y, input_line_theta, path_line_theta):
+    stick_robot.set_data([path[num, 0] - robot.length*np.cos(path[num, 2]), path[num, 0]], 
+                         [path[num, 1] - robot.length*np.sin(path[num, 2]), path[num, 1]])
+    robot_circle.center = (path[num, 0], path[num, 1])
+    grad_arrows.set_offsets([path[num, 0], path[num, 1]])
+    grad_arrows.set_UVC(grad_hist[num, 0], grad_hist[num, 1])
+    input_arrows.set_offsets([path[num, 0], path[num, 1]])
+    input_arrows.set_UVC(input_hist[num, 0], input_hist[num, 1])
+    path_line.set_data(path[:num+1, 0], path[:num+1, 1])
+    input_line_x.set_data(range(num+1), input_hist[:num+1, 0])
+    input_line_y.set_data(range(num+1), input_hist[:num+1, 1])
+    grad_line_x.set_data(range(num+1), grad_hist[:num+1, 0])
+    grad_line_y.set_data(range(num+1), grad_hist[:num+1, 1])
+    input_line_theta.set_data(range(num+1), input_hist[:num+1, 2])
+    path_line_x.set_data(range(num+1), path[:num+1, 0])
+    path_line_y.set_data(range(num+1), path[:num+1, 1])
+    path_line_theta.set_data(range(num+1), path[:num+1, 2])
+    return grad_arrows, input_arrows, stick_robot, robot_circle, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y, input_line_theta, path_line_theta
+
+if robot_type == 1:
+    robot_circle = plt.Circle((robot.x, robot.y), robot.diameter/2, color='orange', fill=True, zorder=2)
+    ax1.add_patch(robot_circle)
+    grad_arrows = ax1.quiver(path[0, 0], path[0, 1], grad_hist[0, 0], grad_hist[0, 1], color='red', scale=0.05, scale_units='xy', alpha=0.7, zorder=3)
+    input_arrows = ax1.quiver(path[0, 0], path[0, 1], input_hist[0, 0], input_hist[0, 1], color='green', scale=0.1, scale_units='xy', alpha=0.5, zorder=3)
+    ani = animation.FuncAnimation(fig, update_r1, frames=len(path), fargs=(path, robot_circle, grad_arrows, input_arrows, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y), interval=dt*100, blit=True, repeat_delay=3000)
+else:
+    stick_robot = plt.Line2D([robot.x - robot.length*np.cos(robot.theta), robot.x], 
+                             [robot.y - robot.length*np.sin(robot.theta), robot.y], color='orange', zorder=2)
+    ax1.add_line(stick_robot)
+    robot_circle = plt.Circle((robot.x, robot.y), 0.1, color='red', fill=True, zorder=2)
+    ax1.add_patch(robot_circle)
+    grad_arrows = ax1.quiver(path[0, 0], path[0, 1], grad_hist[0, 0], grad_hist[0, 1], color='red', scale=0.05, scale_units='xy', alpha=0.7, zorder=3)
+    input_arrows = ax1.quiver(path[0, 0], path[0, 1], input_hist[0, 0], input_hist[0, 1], color='green', scale=0.1, scale_units='xy', alpha=0.5, zorder=3)
+    ani = animation.FuncAnimation(fig, update_r2, frames=len(path), fargs=(path, stick_robot, robot_circle, grad_arrows, input_arrows, path_line, input_line_x, input_line_y, grad_line_x, grad_line_y, path_line_x, path_line_y, input_line_theta, path_line_theta), interval=dt*100, blit=True, repeat_delay=3000)
+
+ax1.set_xlim(-env_limit, env_limit)
+ax1.set_ylim(-env_limit, env_limit)
+ax1.set_xlabel('X-axis')
+ax1.set_ylabel('Y-axis')
+ax1.set_title('Environment Visualization')
+ax1.grid(True)
+ax1.set_aspect('equal', adjustable='box')
+
+plt.show()
---
+++ robots.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class Robot:
+    def __init__(self, x, y, u_x=0, u_y=0, diameter=1):
+        self.x = x
+        self.y = y
+        self.u_x = u_x
+        self.u_y = u_y
+        self.diameter = diameter
+        self.a = 2
+        self.prev_pos = None
+        self.prev_dh = None
+
+    def update(self, dt):
+        self.x += self.u_x * dt
+        self.y += self.u_y * dt
+    
+    def apf_update(self, dt, env, goal=[0, 0], rho=5, k=1, eta_0=1.2, gamma=2, k_rep=1, nu=1):
+        F_x, F_y = env.apf(self.x, self.y, goal, rho, k, eta_0, gamma, k_rep, nu)
+        ### q_dot = F
+        self.u_x = F_x
+        self.u_y = F_y
+        self.update(dt)
+        
+    def cbf_update(self, dt, env, goal=[0, 0], alpha=1, beta=1):
+        F_x, F_y = env.cbf(self.x, self.y, self.diameter, goal, alpha, beta)
+        ### q_dot = F
+        self.u_x = F_x
+        self.u_y = F_y
+        self.update(dt)
+        
+    def bb_grad_descent(self, env, tol=1e-6):
+        dh = list(env.grad_h(self.x, self.y))
+        if np.sqrt(dh[0]**2 + dh[1]**2) < tol:
+            return self.x, self.y
+        if self.prev_dh is not None:
+            if np.sqrt((dh[0] - self.prev_dh[0])**2 + (dh[1] - self.prev_dh[1])**2) > tol:
+                ### Geometric mean of a_long and a_short
+                self.a = np.sqrt((self.x - self.prev_pos[0])**2 + (self.y - self.prev_pos[1])**2) / np.sqrt((dh[0] - self.prev_dh[0])**2 + (dh[1] - self.prev_dh[1])**2)
+        x = self.x + self.a * dh[0]
+        y = self.y + self.a * dh[1]
+        self.prev_pos = [self.x, self.y]
+        self.prev_dh = dh
+        return x, y
+
+
+class StickRobot(Robot):
+    def __init__(self, x, y, theta=0, length=1.5, u_x=0, u_y=0, u_theta=0):
+        super().__init__(x, y, u_x, u_y, diameter=0)
+        self.length = length
+        self.u_theta = u_theta
+        self.theta = theta
+
+    def update(self, dt):
+        super().update(dt)
+        self.theta += self.u_theta * dt
+        
+    def apf_update(self, dt, env, goal=[0, 0], rho=5, k=1, eta_0=1.2, gamma=2, k_rep=1, nu=1, mul_theta=2, tail_orientation=0, final_heading_k=0.5):
+        x0 = self.x - self.length * np.cos(self.theta)
+        y0 = self.y - self.length * np.sin(self.theta)
+        F_x, F_y = env.apf(x0, y0, goal, rho, tail_orientation*k, mul_theta*eta_0, gamma, mul_theta*k_rep, nu=0)
+        self.u_theta = np.clip((F_x * np.sin(self.theta) - F_y * np.cos(self.theta)) * self.length - final_heading_k * self.theta, -np.pi/3, np.pi/3)
+        super().apf_update(dt, env, goal, rho, k, eta_0, gamma, k_rep, nu)
+        
+    def cbf_update(self, dt, env, goal=[0, 0], alpha=1, beta=1, tail_orientation=-1):
+        #### TODO: DOES NOT WORK
+        x0 = self.x - self.length * np.cos(self.theta)
+        y0 = self.y - self.length * np.sin(self.theta)
+        F_x, F_y = env.cbf(x0, y0, 0, goal, alpha, beta*tail_orientation)
+        self.u_theta = (F_x * np.sin(self.theta) - F_y * np.cos(self.theta))
+        super().cbf_update(dt, env, goal, alpha, beta)
+        
+        
